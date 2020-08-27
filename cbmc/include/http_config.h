bc7c2da3343d4056ed5b0bb9e1a924eec4dcb06c
SarenaAWS <6563840+sarenameas@users.noreply.github.com>
2020-08-27 16:42:09 -0700

    Update CBMC symlinks and makefile paths. (#4)

--- cbmc/include/http_config.h
+++ cbmc/include/http_config.h
@@ -22,30 +22,6 @@
 #ifndef HTTP_CONFIG_H
 #define HTTP_CONFIG_H
 
-/**************************************************/
-/******* DO NOT CHANGE the following order ********/
-/**************************************************/
-
-/* Include logging header files and define logging macros in the following order:
- * 1. Include the header file "logging_levels.h".
- * 2. Define the LIBRARY_LOG_NAME and LIBRARY_LOG_LEVEL macros depending on
- * the logging configuration for HTTP.
- * 3. Include the header file "logging_stack.h", if logging is enabled for HTTP.
- */
-
-#include "logging_levels.h"
-
-/* Logging configuration for the HTTP library. */
-#ifndef LIBRARY_LOG_NAME
-    #define LIBRARY_LOG_NAME    "HTTP"
-#endif
-
-#ifndef LIBRARY_LOG_LEVEL
-    #define LIBRARY_LOG_LEVEL    LOG_NONE
-#endif
-
-#include "logging_stack.h"
-
-/************ End of logging configuration ****************/
+/* Empty configuration header simply for compilation. */
 
 #endif /* ifndef HTTP_CONFIG_H */
