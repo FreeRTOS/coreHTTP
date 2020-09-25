fde8f50d07addedf8ae19977415086998d5503e7
Sarena Meas <sarem@amazon.com>
2020-09-25 00:52:57 -0700

    Make http-parser header public for customers to use.

--- httpFilePaths.cmake
+++ httpFilePaths.cmake
@@ -7,17 +7,11 @@
 
 # HTTP library source files.
 set( HTTP_SOURCES
-     ${CMAKE_CURRENT_LIST_DIR}/source/core_http_client.c )
+     ${CMAKE_CURRENT_LIST_DIR}/source/core_http_client.c
+     ${CMAKE_CURRENT_LIST_DIR}/source/third-party/http_parser/http_parser.c )
 
 # HTTP library public include directories.
 set( HTTP_INCLUDE_PUBLIC_DIRS
      ${CMAKE_CURRENT_LIST_DIR}/source/include
-     ${CMAKE_CURRENT_LIST_DIR}/source/portable )
-
-# HTTP library private include directories.
-set( HTTP_INCLUDE_PRIVATE_DIRS
+     ${CMAKE_CURRENT_LIST_DIR}/source/portable
      ${CMAKE_CURRENT_LIST_DIR}/source/third-party/http_parser )
-
-# HTTP library private sources.
-set( HTTP_PRIVATE_SOURCES
-     ${CMAKE_CURRENT_LIST_DIR}/source/third-party/http_parser/http_parser.c )
