2f24ccc8ab631abcc078fc33cd272222db376c87
Nicholas Rodgers <nrdg@amazon.com>
2020-10-05 15:33:06 -0400

    Rename proof markers to cbmc-proof.txt

--- test/cbmc/proofs/run-cbmc-proofs.py
+++ test/cbmc/proofs/run-cbmc-proofs.py
@@ -23,7 +23,7 @@ import pathlib
 import subprocess
 import sys
 
-PROOF_TOUCH_FILE_NAME = "wellspring.txt"
+PROOF_TOUCH_FILE_NAME = "cbmc-proof.txt"
 
 DESCRIPTION = "Configure and run all CBMC proofs in parallel"
 # Keep this hard-wrapped at 70 characters, as it gets printed verbatim
