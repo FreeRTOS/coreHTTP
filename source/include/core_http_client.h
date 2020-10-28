6316a02e7875f8b0e4b69a039a5366d36d8fc0cf
SarenaAWS <6563840+sarenameas@users.noreply.github.com>
2020-10-28 11:58:58 -0700

    Fix spaces again.

--- source/include/core_http_client.h
+++ source/include/core_http_client.h
@@ -800,9 +800,9 @@ HTTPStatus_t HTTPClient_Send( const TransportInterface_t * pTransport,
  *
  * The location, within #HTTPResponse_t.pBuffer, of the value found, will be
  * returned in @p pValue. If the header value is empty for the found @p pField,
- * then this function will return #HTTPSuccess, and set the values for @p pValue
- * and @p ValueLen as NULL and zero respectively. According to RFC 2616, it is not
- * invalid to have an empty value for some header fields.
+ * then this function will return #HTTPSuccess, and set the values for
+ * @p pValueLoc and @p pValueLen as NULL and zero respectively. According to
+ * RFC 2616, it is not invalid to have an empty value for some header fields.
  *
  * @note This function should only be called on a complete HTTP response. If the
  * request is sent through the #HTTPClient_Send function, the #HTTPResponse_t is
