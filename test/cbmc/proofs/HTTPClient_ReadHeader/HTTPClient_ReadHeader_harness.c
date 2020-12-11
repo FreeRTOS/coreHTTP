a8f107099f227e79bfeec7c0cbbd406a86858daf
Sarena Meas <sarem@amazon.com>
2020-12-11 15:57:22 -0800

    Update version 1.0.0 to 2.0.0.

--- test/cbmc/proofs/HTTPClient_ReadHeader/HTTPClient_ReadHeader_harness.c
+++ test/cbmc/proofs/HTTPClient_ReadHeader/HTTPClient_ReadHeader_harness.c
@@ -1,5 +1,5 @@
 /*
- * coreHTTP v1.0.0
+ * coreHTTP v2.0.0
  * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
