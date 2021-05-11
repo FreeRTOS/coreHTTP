46cbde36a04163d33e3d86494182615574c1ed21
Aniruddha Kanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2021-05-11 11:23:24 -0700

    Revert all the spelling errors

--- source/include/core_http_client_private.h
+++ source/include/core_http_client_private.h
@@ -55,17 +55,14 @@
 #define DASH_CHARACTER                     '-'                                            /**< A dash character macro to help with serializing a request. */
 #define DASH_CHARACTER_LEN                 ( 1U )                                         /**< The length of #DASH_CHARACTER. */
 
-/* TestComment. */
 /* Constants for HTTP header copy checks. */
 #define CARRIAGE_RETURN_CHARACTER          '\r' /**< A carriage return character to help with header validation. */
 #define LINEFEED_CHARACTER                 '\n' /**< A linefeed character to help with header validation. */
 #define COLON_CHARACTER                    ':'  /**< A colon character to help with header validation. */
 
-/* IsThisCaught? */
-
 /**
  * @brief Indicator for function #httpHeaderStrncpy that the pSrc parameter is a
- * header value. AddOneMoreSpellingError.
+ * header value.
  */
 #define HTTP_HEADER_STRNCPY_IS_VALUE       0U
 
